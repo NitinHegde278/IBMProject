b3c2f83cadca4ecad06d57ec52217ca5d161f816
nitin <nhnitinhegde24@gmail.com>
2020-04-20 10:33:32 +0530

    Edited on 20-Apr

--- online-shopping-project/src/main/java/com/training/ibm/bean/Cart.java
+++ online-shopping-project/src/main/java/com/training/ibm/bean/Cart.java
@@ -8,59 +8,71 @@ import javax.persistence.Id;
 @Entity
 public class Cart {
 
-	 @Id
-	 @GeneratedValue(strategy=GenerationType.IDENTITY)
-	 Integer cartId;
-	 
-	 private Integer productId;
-	 private String productCategory;
-	 private String productName;
-	 private double productPrice;
-	 private double subTotal;
-	 private Integer productQuantity;
-	 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer cartId;
+
+	private Integer productId;
+	private String productCategory;
+	private String productName;
+	private double productPrice;
+	private double subTotal;
+	private Integer productQuantity;
+
 	public Integer getCartId() {
 		return cartId;
 	}
+
 	public void setCartId(Integer cartId) {
 		this.cartId = cartId;
 	}
+
 	public Integer getProductId() {
 		return productId;
 	}
+
 	public void setProductId(Integer productId) {
 		this.productId = productId;
 	}
+
 	public String getProductCategory() {
 		return productCategory;
 	}
+
 	public void setProductCategory(String productCategory) {
 		this.productCategory = productCategory;
 	}
+
 	public String getProductName() {
 		return productName;
 	}
+
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
+
 	public double getProductPrice() {
 		return productPrice;
 	}
+
 	public void setProductPrice(double productPrice) {
 		this.productPrice = productPrice;
 	}
+
 	public double getSubTotal() {
 		return subTotal;
 	}
+
 	public void setSubTotal(double subTotal) {
 		this.subTotal = subTotal;
 	}
+
 	public Integer getProductQuantity() {
 		return productQuantity;
 	}
+
 	public void setProductQuantity(Integer productQuantity) {
 		this.productQuantity = productQuantity;
 	}
-	 
-	 
+
 }
--- online-shopping-project/src/main/java/com/training/ibm/bean/Product.java
+++ online-shopping-project/src/main/java/com/training/ibm/bean/Product.java
@@ -17,43 +17,53 @@ public class Product {
 	private String productName;
 	private double productPrice;
 	private Integer unitStock;
-	
+
 	public Integer getProductId() {
 		return productId;
 	}
+
 	public void setProductId(Integer productId) {
 		this.productId = productId;
 	}
+
 	public String getProductCategory() {
 		return productCategory;
 	}
+
 	public void setProductCategory(String productCategory) {
 		this.productCategory = productCategory;
 	}
+
 	public String getProductDescription() {
 		return productDescription;
 	}
+
 	public void setProductDescription(String productDescription) {
 		this.productDescription = productDescription;
 	}
+
 	public String getProductName() {
 		return productName;
 	}
+
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
+
 	public double getProductPrice() {
 		return productPrice;
 	}
+
 	public void setProductPrice(double productPrice) {
 		this.productPrice = productPrice;
 	}
+
 	public Integer getUnitStock() {
 		return unitStock;
 	}
+
 	public void setUnitStock(Integer unitStock) {
 		this.unitStock = unitStock;
 	}
 
-	
 }

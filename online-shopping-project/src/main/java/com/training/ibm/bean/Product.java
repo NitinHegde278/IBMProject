faea90e28f25c0666072091c707e47cca93d0d2b
nitin <nhnitinhegde24@gmail.com>
2020-04-20 09:40:29 +0530

    Edited on 20-Apr

--- online-shopping-project/src/main/java/com/training/ibm/bean/Product.java
+++ online-shopping-project/src/main/java/com/training/ibm/bean/Product.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class Product {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Integer productId;
 
 	private String productCategory;
